aa593800a08eeb32a8de2f83759b700f8d2b1a82
Joni Iswanto <joniiswa1101@gmail.com>
2019-07-30 11:13:10 +0700

    Tugas 2 ReactJS
    function, export dan import, props pada class dan fucntion, state & props

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Header from "./Page/Header";
+import MenuUtama from "./Page/MenuUtama";
+import MenuMakanan from "./Page/MenuMakanan";
+import MenuTentangKami from "./Page/MenuTentangKami";
+import MenuKontak from "./Page/MenuKontak";
+import Footer from "./Page/Footer";
+
+class App extends Component {
+    render () {
+      return (
+        <div>
+          <Header />
+          <MenuUtama />
+          <MenuMakanan />
+          <MenuTentangKami />
+          <MenuKontak />
+          <Footer />
+
+        </div>
+      );
+    }
+}
+export default App;
